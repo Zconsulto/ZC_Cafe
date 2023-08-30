edbc08e30fa74f3e6083282fbe23ec87872de972
Zconsulto <ashorbagy@zconsulto.com>
2023-08-30 15:32:42 -0400

    Modified the main method to open on the login form

--- CafeMGmtSys/CafeMGmtSys/Program.cs
+++ CafeMGmtSys/CafeMGmtSys/Program.cs
@@ -11,7 +11,7 @@ namespace CafeMGmtSys
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ItemsForm());
+            Application.Run(new Form1());
         }
     }
 }
